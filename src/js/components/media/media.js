0c9488f417e9bee8f4c9af6a45727c915ebd1f2c
Randall Wilk <wilk.randall@gmail.com>
2018-07-01 12:08:46 -0500

    Update media component

--- src/js/components/media/media.js
+++ src/js/components/media/media.js
@@ -3,49 +3,49 @@ import MediaBody from './media-body';
 import MediaAside from './media-aside';
 
 export const props = {
-	tag: {
-		type: String,
-		default: 'div'
-	},
-	rightAlign: {
-		type: Boolean,
-		default: false
-	},
-	verticalAlign: {
-		type: String,
-		default: 'top'
-	},
-	noBody: {
-		type: Boolean,
-		default: false
-	}
+    noBody: {
+        type: Boolean,
+        default: false
+    },
+    rightAlign: {
+        type: Boolean,
+        default: false
+    },
+    tag: {
+        type: String,
+        default: 'div'
+    },
+    verticalAlign: {
+        type: String,
+        default: 'top'
+    }
 };
 
 export default {
-	functional: true,
+    functional: true,
 
-	props,
+    props,
 
-	render (h, { props, data, slots, children }) {
-		let childNodes = props.noBody ? children : [];
-		const $slots = slots();
+    render (h, { props, data, slots, children }) {
+        let childNodes = props.noBody ? children : [];
+        const $slots = slots();
 
-		if (! props.noBody) {
-			if ($slots.aside && !props.rightAlign) {
-				childNodes.push(
-					h(MediaAside, { staticClass: 'mr-3', props: { verticalAlign: props.verticalAlign } }, $slots.aside)
-				);
-			}
+        if (! props.noBody) {
+            if ($slots.aside && ! props.rightAlign) {
+                childNodes.push(
+                    h(MediaAside, { staticClass: 'mr-3', props: { verticalAlign: props.verticalAlign } }, $slots.aside)
+                );
+            }
 
-			childNodes.push(h(MediaBody, $slots.default));
+            childNodes.push(h(MediaBody, $slots.default));
 
-			if ($slots.aside && props.rightAlign) {
-				childNodes.push(
-					h(MediaAside, { staticClass: 'ml-3', props: { verticalAlign: props.verticalAlign } }, $slots.aside)
-				);
-			}
-		}
+            if ($slots.aside && props.rightAlign) {
+                childNodes.push(
+                    h(MediaAside, { staticClass: 'ml-3', props: { verticalAlign: props.verticalAlign } }, $slots.aside)
+                );
+            }
+        }
 
-		return h(props.tag, mergeData(data, { staticClass: 'media' }), childNodes);
-	}
+        return h(props.tag, mergeData(data, { staticClass: 'media' }), childNodes);
+    }
 };
